8e3f96921603515b8638650b2307da130ff2dfeb
Louis-de-Lavenne-de-Choulot <louis.delavenne@algosup.com>
2023-05-26 14:51:48 +0200

    changes

--- AlgorithmsTestProject/ArraySorts.cs
+++ AlgorithmsTestProject/ArraySorts.cs
@@ -51,34 +51,98 @@
                 {
                     if (array[i - 1] > array[i])
                     {
-                        ArrayProblems.Swap(array, i-1, i);
+                        ArrayProblems.Swap(array, i - 1, i);
                         swapped = true;
                     }
                 }
 
                 --n;
-            } 
+            }
             while (swapped && n > 0);
         }
 
         public static void ShuffleSort(int[] array)
         {
-            throw new NotImplementedException();
+            var random = new Random();
+            for (var i = 0; i < array.Length; i++)
+            {
+                var j = random.Next(i, array.Length);
+                ArrayProblems.Swap(array, i, j);
+            }
         }
 
         public static void IntroSort(int[] array)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < array.Length; i++)
+            {
+                var min = i;
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+                ArrayProblems.Swap(array, i, min);
+            }
         }
 
         public static void CocktailSort(int[] array)
         {
-            throw new NotImplementedException();
+            var sorted = false;
+            while (!sorted)
+            {
+                sorted = true;
+                for (var i = 1; i < array.Length; i++)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        ArrayProblems.Swap(array, i - 1, i);
+                        sorted = false;
+                    }
+                }
+                for (var i = array.Length - 1; i > 0; i--)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        ArrayProblems.Swap(array, i - 1, i);
+                        sorted = false;
+                    }
+                }
+            }
         }
 
         public static void QuickSort(int[] array)
         {
-            throw new NotImplementedException();
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        private static void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int partitionIndex = Partition(array, left, right);
+                QuickSort(array, left, partitionIndex - 1);
+                QuickSort(array, partitionIndex + 1, right);
+            }
+        }
+
+        private static int Partition(int[] array, int left, int right)
+        {
+            int pivot = array[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    ArrayProblems.Swap(array, i, j);
+                }
+            }
+
+            ArrayProblems.Swap(array, i + 1, right);
+            return i + 1;
         }
 
         public static void BlockSort(int[] array)

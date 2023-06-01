0ba1353d3bf867f75ac6f7d8d34440a474754920
Louis-de-Lavenne-de-Choulot <louis.delavenne@algosup.com>
2023-06-01 14:29:15 +0200

    Update ConsListProblems.cs

--- AlgorithmsTestProject/ConsListProblems.cs
+++ AlgorithmsTestProject/ConsListProblems.cs
@@ -5,12 +5,53 @@ public static class ConsListProblems
     public static IConsList<T> InsertBefore<T>(this IConsList<T> self, 
         int index, T value)
     {
-        throw new NotImplementedException();
+        // [:index -1] + [value] + [index:]
+        var newList = new List<T>();
+        var i = 0;
+        while (self != null)
+        {
+            if (i < index-1)
+            {
+                newList.Append(self.Value);
+            }
+            else if (i == index-1)
+            {
+                newList.Append(value);
+            }
+            else
+            {
+                newList.Append(self.Value);
+            }
+            self.NextValue();
+            i++;
+        };
+        return newList.ToConsList();
     }
 
     public static IConsList<T> RemoveAt<T>(this IConsList<T> self, 
         int index)
     {
-        throw new NotImplementedException();
+        // [:index -1] + [index:]
+        var newList = new List<T>();
+        var i = 0;
+        while (self != null)
+        {
+            if (i < index-1)
+            {
+                newList.Append(self.Value);
+            }
+            else if (i == index-1)
+            {
+                // skip
+            }
+            else
+            {
+                newList.Append(self.Value);
+            }
+            self.NextValue();
+            i++;
+        };
+        return newList.ToConsList();
+        // throw new NotImplementedException();
     }
 }
